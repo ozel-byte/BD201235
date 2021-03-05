8ac20b9f50737f4c90060637c15230bfe0d43085
Kevin-KHA <201378@ids.upchiapas.edu.mx>
2021-03-04 22:14:29 -0600

    se agregan metodos registrar, eliminar, modificar a la clase DuenoDAO.java

--- src/persistencia/DuenoDAO.java
+++ src/persistencia/DuenoDAO.java
@@ -1,11 +1,10 @@
 package persistencia;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -39,5 +38,48 @@ public class DuenoDAO {
     }
 
 
+    public Dueno agregarDueno()
+    {
+        Session session = factory.openSession();
+        session.beginTransaction();
+        //instancia pa iniciar el registro
+        Dueno registro = new Dueno();
 
+        //Agregando
+        registro.setNombre("x3");
+        registro.setCorreo("ejemplo xd");
+        registro.setDireccion("av abc");
+        registro.setTelefono("96123456");
+
+        //se guarda el objeto en la base de datos
+        session.save(registro);
+        session.getTransaction().commit();
+        session.close();
+
+        return registro;
+    }
+
+    public void modificar()
+    {
+        Dueno cambiar = new Dueno();
+
+        cambiar.setNombre("hola");
+
+
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+        session.save(cambiar);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void deleteDueno(int id){
+        Session session = factory.openSession();
+        Transaction tx = null;
+        tx = session.beginTransaction();
+        Dueno dao = (Dueno) session.get(Dueno.class, id);
+        System.out.println(dao.getNombre());
+        session.delete(dao);
+        tx.commit();
+    }
 }
