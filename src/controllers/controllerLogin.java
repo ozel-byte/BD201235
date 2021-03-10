f336d8669764b73d52cddd0cc44311a8ccf9892a
ozel <ozelcode@gmail.com>
2021-03-09 22:32:30 -0600

    Se agrego un loading al login

--- src/controllers/controllerLogin.java
+++ src/controllers/controllerLogin.java
@@ -1,18 +1,19 @@
 package controllers;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import persistencia.Usuario;
 import persistencia.UsuarioDAO;
+
+
 
 public class controllerLogin {
 
@@ -33,8 +34,52 @@ public class controllerLogin {
 
     @FXML
     private Button buttonsignin;
+    @FXML
+    private ProgressIndicator loading;
+    Usuario resp;
 
+    Task task = new Task<Void>() {
+        @Override
+        public Void call(){
+            UsuarioDAO userdao = new UsuarioDAO();
+            resp = userdao.getUsuario(inputnombre.getText(),inputpassword.getText());
+            return null;
+        }
 
+        @Override
+        protected void succeeded() {
+            super.succeeded();
+            if (resp!=null){
+                System.out.println("entro aqui");
+                if (resp.getRol().equals("admin")){
+                    System.out.println("aqui 2");
+                    ventananew("../view/Admin.fxml");
+                }else if (resp.getRol().equals("user")){
+                    ventananew("../view/Home.fxml");
+                }
+
+            }else {
+                 msgUserError.setVisible(true);
+            }
+            updateMessage("done!");
+
+        }
+    };
+
+    public void ventananew(String ruta){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(ruta));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+            System.out.println("aqui33");
+            Stage stage2 = (Stage) buttonsignin.getScene().getWindow();
+            stage2.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 
     @FXML
     public void iniciarSesion(){
@@ -61,41 +106,13 @@ public class controllerLogin {
         });
 
         if (inputnombre.getText().length()>0 && inputpassword.getText().length()>0){
-            Usuario resp;
-            UsuarioDAO userdao = new UsuarioDAO();
-          resp = userdao.getUsuario(inputnombre.getText(),inputpassword.getText());
-          if (resp!=null){
-              if (resp.getRol().equals("admin")){
-                  try{
-                      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Admin.fxml"));
-                      Parent root1 = (Parent) fxmlLoader.load();
-                      Stage stage = new Stage();
-                      stage.setScene(new Scene(root1));
-                      stage.show();
-                      Stage stage2 = (Stage) buttonsignin.getScene().getWindow();
-                      stage2.close();
-                  }catch (Exception e){
 
-                  }
-              }else if (resp.getRol().equals("user")){
-                  try {
+            Thread t = new Thread(task);
+            t.setDaemon(true);
+            t.start();
+            loading.setVisible(true);
 
-                      System.out.println("entro---");
-                      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Home.fxml"));
-                      Parent root1 = (Parent) fxmlLoader.load();
-                      Stage stage = new Stage();
-                      stage.setScene(new Scene(root1));
-                      stage.show();
-                      Stage stage2 = (Stage) buttonsignin.getScene().getWindow();
-                      stage2.close();
-                  }catch (Exception e){
-                      System.out.println(e);
-                  }
-              }
 
-          }else {
-              msgUserError.setVisible(true);
-          }
         }else{
             if (inputnombre.getText().length()==0 && inputpassword.getText().length()==0){
                 msgErrorNombre.setVisible(true);
